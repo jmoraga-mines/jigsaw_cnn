ec48412800f11ea2fe8cba42745face52b241eca
Jaime "Jim" Moraga <47835974+jmoraga-mines@users.noreply.github.com>
2019-08-12 15:24:28 -0600

    Update wendian-gpu.sh

--- wendian-gpu.sh
+++ wendian-gpu.sh
@@ -1,6 +1,4 @@
 # Single Node to compile
 srun -N 1 -p gpu -t 144:00:00 --mem 32G  --exclusive --pty bash 
 
-#export LD_LIBRARY_PATH=/sw/libs/cuda/10.1/extras/CUPTI/lib64:/sw/libs/cuda/10.1/lib64:/scratch/jmoraga/cuda-10/lib64
-
-#srun -N 1 -p gpu -t 48:00:00 --mem 32G ./darknet detector train cfg/edgar.data cfg/edgar.cfg backup/edgar_last.weights -gpus 0,1,2,3
+#export LD_LIBRARY_PATH=/sw/libs/cuda/10.1/extras/CUPTI/lib64:/sw/libs/cuda/10.1/lib64
